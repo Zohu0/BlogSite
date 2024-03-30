67245c211bf54f98d1cbe5932991f0217ba65dab
Zohu0 <shaikhzohaib477@gmail.com>
2024-03-31 02:48:54 +0530

    Added Navbar

--- app.js
+++ app.js
@@ -5,89 +5,92 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const Blog = require("./models/blog.js")
+const Blog = require("./models/blog.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/blog";
-
+const MONGO_URL = 'mongodb://127.0.0.1:27017/blog';
 
 const main = async () => {
     await mongoose.connect(MONGO_URL);
 };
 
 main().then(() => { 
-    console.log("Connected to db")
+    console.log("Connected to db");
 }).catch((err)=>{
-    console.log(err)
-})
+    console.error("Error connecting to db:", err);
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")))
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Home route
-app.get("/", async(req, res) => {
-    const allBlogs = await Blog.find({})
-    res.render("blogs/home.ejs", {allBlogs})
+app.get("/", async (req, res) => {
+    try {
+        const allBlogs = await Blog.find({});
+        res.render("blogs/home.ejs", {allBlogs});
+    } catch (error) {
+        console.error("Error rendering home page:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
-
 
 // Blog route
 app.get("/blogs", async (req, res) => {
-    const allBlogs = await Blog.find({})
-    res.render("blogs/blog.ejs", {allBlogs})
-})
-
-
-// Add blog route
-app.get("/blogs/addblog", (req, res) => {
-    res.render("blogs/addblog.ejs")
+    try {
+        const allBlogs = await Blog.find({});
+        res.render("blogs/blog.ejs", {allBlogs});
+    } catch (error) {
+        console.error("Error rendering blog page:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
-
 
 // Blog show route
 app.get("/blogs/:id", async (req, res) => {
-    let {id} = req.params;
-    const blog = await Blog.findById(id);
-    res.render("blogs/blogshow.ejs", {blog})
-})
+    try {
+        const { id } = req.params;
+        const blog = await Blog.findById(id);
+        if (!blog) {
+            return res.status(404).send("Blog not found");
+        }
+        res.render("blogs/blogshow.ejs", { blog });
+    } catch (error) {
+        console.error("Error fetching blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
+// Addblog route
+app.get("/blogs/addblog", (req, res) => {
+    res.render("blogs/addblog.ejs");
+});
 
-//Home latest blog show route
-app.get("/:id", async (req, res) => {
-    let {id} = req.params;
-    const blog = await Blog.findById(id);
-    res.render("blogs/latestshow.ejs", {blog})
-})
-
-
-// Adding Blog Route
+// AddingBlog Route
 app.post("/blogs", async (req, res) => {
-    const newBlog = new Blog(req.body.blog);
-    await newBlog.save();
-    res.redirect("/blogs");
-})
+    try {
+        const newBlog = new Blog(req.body.blog);
+        await newBlog.save();
+        res.redirect("/blogs");
+    } catch (error) {
+        console.error("Error adding new blog:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
-
-
-
-
-
-// app.get("/testblog", async (req,res)=>{
-//     let sampleListing = new Blog({
-//         title: "My new villa",
-//         author: "Near the beach",
-//         content: "Hey this this test",
-//     })
-
-//     await sampleListing.save()
-//     console.log("sample was saved")
-//     res.send("Successfull testing");
-// });
-
+// Home latest blog show route
+app.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const blog = await Blog.findById(id);
+        res.render("blogs/latestshow.ejs", { blog });
+    } catch (error) {
+        console.error("Error rendering latest blog page:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  let ul = document.querySelector('.navbar ul');
+  ul.classList.toggle('active');
+  let container = document.querySelector('.container');
+  container.classList.toggle('content');
+}
+
+
+
